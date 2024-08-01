2d4581e4a1c16dd86bb7ffb081732a88b2506c23
BonSik <rnqhstlr2297@gmail.com>
2024-08-01 21:44:00 +0900

    초기 세팅

---
+++ src/test/java/com/example/weski/WeskiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeskiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
